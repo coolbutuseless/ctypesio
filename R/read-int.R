6e8cb79f5ddd773833b4b9c34788eafc2dc13fbc
mike <mikefc@coolbutuseless.com>
2024-10-06 16:37:25 +1000

    bounds check when reading uint64 into bit64::integer64

--- R/read-int.R
+++ R/read-int.R
@@ -188,6 +188,11 @@ read_uint64 <- function(con, n = 1, endian = NULL, promote = NULL, bounds_check 
     res <- raw_to_hex(raw_vec, size = 8, endian = endian)
   } else if (promote == 'bit64') {
     res <- .Call(raw_to_integer64_, raw_vec, big_endian = (endian == 'big'))
+    # bit64::integer64 is a signed type.
+    # So if we ever get back a -ve number when trying to read an uint64, it
+    # means that the value is above 2^63 and not properly representable
+    # in this data type
+    do_bounds_check(res, bounds_check, lo = 0, hi = Inf, lo_str = "0", hi_str = "2^64")
   } else {
     stop("Unknown promotion method: ", promote)
   }
